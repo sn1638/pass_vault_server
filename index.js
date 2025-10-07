cc06621e4a0710247fbde06b61b220e34f78cac8
sn1638 <sachin111mrj@gmail.com>
2025-10-07 09:40:48 +0530

    fixed start issue

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Mongodb connected successfully");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB error:", err));
+
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
