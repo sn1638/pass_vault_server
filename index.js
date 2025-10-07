fba78b48d2e41a54f1d3fc92205cb56d0d7b17df
SACHIN KUMAR VERMA <sachin111mrj@gmail.com>
2025-10-07 13:39:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/vault", vaultRoutes);
 
-app.get("/",()=>{
-  res.send("hello world")
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Mongodb connected successfully");
+    console.log("MongoDB connected successfully");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB error:", err));
-
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
